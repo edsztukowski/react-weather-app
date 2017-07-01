6b0c7f91dcc7720e7a8cf58e7313c5d1bd6c2ad1
EDWARD <edsztukowski@gmail.com>
2017-07-01 12:18:39 -0600

    Started refactoring the search component and adding props.

--- app/components/search.js
+++ app/components/search.js
@@ -2,12 +2,25 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipcode: 80303
+    };
+  }
   render() {
     return (
       <div className="search-bar">
         <form>
-          <input type="text" name="searchQuery"/>
-          <input type="submit" value="Get Weather"/>
+          <input
+            type="text"
+            name="searchQuery"
+            value={this.state.zipcode}
+          />
+          <input
+            type="submit"
+            value="Get Weather"
+          />
         </form>
       </div>
     )
