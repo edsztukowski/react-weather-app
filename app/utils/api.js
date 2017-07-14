7483cc754cb758b0cc249d495ee3f2209e5f8c68
EDWARD <edsztukowski@gmail.com>
2017-07-13 18:00:34 -0600

    Added api call for five day forecast

--- app/utils/api.js
+++ app/utils/api.js
@@ -7,7 +7,17 @@ module.exports = {
 
     return axios.get(encodedURI)
       .then(function(response){
-        return response.data;
+        return response;
+      })
+  },
+
+  fetchFiveDays: function(zipcode) {
+    var apiKey = 'a62f7a63749676dd43fbfc9700bdf15c';
+    var encodedURI = window.encodeURI('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + zipcode + '&type=accurate&APPID=' + apiKey + '&cnt=5');
+
+    return axios.get(encodedURI)
+      .then(function(response){
+        return response;
       })
   }
 }
